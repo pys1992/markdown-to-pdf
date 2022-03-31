58bdbdcb603f7fe51b3b08ff10cb8d04fc5025fc
psy <me@pys.im>
2022-03-31 21:33:08 +0800

    feat: set title to untitled when print

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 navigator.serviceWorker.register('/sw.js');
+
+document.addEventListener('keydown', event => {
+    let meta = navigator.userAgent.indexOf('Mac OS X') ? 'metaKey' : 'ctrlKey';
+
+    if (event[meta] && event.key == 'p') {
+        event.preventDefault();
+        let title = document.title;
+        document.title = 'untitled';
+        window.print();
+        document.title = title;
+    }
+})
+
